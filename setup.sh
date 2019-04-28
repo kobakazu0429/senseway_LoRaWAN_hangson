2c34ed45e0a0324f0a93b71c347d77f6250ddbab
kobakakzu0429 <kobakazu10@gmail.com>
2019-04-28 17:01:38 +0900

    fix

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR=$(cd $(dirname $0) && pwd)
-
-echo "Create Libs Symbolic Links"
-echo "==========================================="
-
-echo "Create Libs Symbolic Links : done"
-echo "==========================================="
